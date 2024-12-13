32ac349afee23fe24690cb5905b3b993f4bcfc71
Sardorbek Mamatov <1sardormamatov@gmail.com>
2024-12-13 10:11:21 +0500

    Remove all comments in main.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -2,27 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:online_course/screens/home_Screen.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    //Set the fit size (Find your UI design, look at the dimensions of the device screen and fill it in,unit in dp)
     return ScreenUtilInit(
       designSize: const Size(360, 690),
       minTextAdapt: true,
       splitScreenMode: true,
-      // Use builder only if you need to use library outside ScreenUtilInit context
       builder: (_, child) {
         return MaterialApp(
           title: 'First Method',
-          // You can use the library anywhere in the app even in theme
-          theme: ThemeData(
-              // primarySwatch: Colors.blue,
-              // textTheme: Typography.englishLike2018.apply(fontSizeFactor: 1.sp),
-              ),
+          theme: ThemeData(),
           home: child,
         );
       },
