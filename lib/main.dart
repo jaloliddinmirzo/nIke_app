4652e43b848d972bbc77616089f82f13b7ce85ef
Sardorbek Mamatov <1sardormamatov@gmail.com>
2024-12-13 10:22:56 +0500

    Add test file with the name new_widget.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -2,28 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:online_course/screens/home_Screen.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    
     return ScreenUtilInit(
       designSize: const Size(360, 690),
       minTextAdapt: true,
       splitScreenMode: true,
-      
       builder: (_, child) {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           title: 'First Method',
-          
-          theme: ThemeData(
-              
-              
-              ),
+          theme: ThemeData(),
           home: child,
         );
       },
