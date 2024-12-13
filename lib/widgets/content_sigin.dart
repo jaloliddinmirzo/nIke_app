0649c48d7f73955da911119454f5285200b64a65
Ibrokhimovasss <sanobaribroximova@gmail.com>
2024-12-13 09:09:28 +0300

    lobardan birlashdii

--- lib/widgets/content_sigin.dart
+++ lib/widgets/content_sigin.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:online_course/utils/app_colors.dart';
-import 'package:online_course/widgets/button_push.dart';
-import 'package:online_course/widgets/inputs/custom_text_field.dart';
-import 'package:online_course/widgets/inputs/password_input.dart';
-import 'package:online_course/widgets/texts.dart';
+import '../utils/app_colors.dart';
+import '../widgets/button_push.dart';
+import '../widgets/inputs/custom_text_field.dart';
+import '../widgets/inputs/password_input.dart';
+import '../widgets/texts.dart';
 
 class ContentSigin extends StatefulWidget {
   const ContentSigin({super.key});
--- lib/widgets/inputs/password_input.dart
+++ lib/widgets/inputs/password_input.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:online_course/utils/app_colors.dart';
+import '../../utils/app_colors.dart';
 
 class PasswordField extends StatefulWidget {
   @override
